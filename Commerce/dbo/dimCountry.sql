e350304629dde03d431db73cf3b9a683da52905d
SQLSprint <andrew@coeo.com>
2018-10-01 13:48:39 +0100

    Initial copy to source code control.

---
+++ Commerce/dbo/dimCountry.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[dimCountry]
+(
+	[CountryKey] INT NOT NULL CONSTRAINT PK_dimCountry PRIMARY KEY,
+	Country VARCHAR(24) NOT NULL CONSTRAINT UK_dimCountry_Country UNIQUE
+)
---
+++ Commerce/staging/Tables/Sale.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [staging].[Sale] (
+    [Segment]             NVARCHAR (255) NULL,
+    [Country]             NVARCHAR (255) NULL,
+    [Product]             NVARCHAR (255) NULL,
+    [Discount Band]       NVARCHAR (255) NULL,
+    [Units Sold]          FLOAT (53)     NULL,
+    [Manufacturing Price] MONEY          NULL,
+    [Sale Price]          MONEY          NULL,
+    [Gross Sales]         MONEY          NULL,
+    [Discounts]           MONEY          NULL,
+    [ Sales]              MONEY          NULL,
+    [COGS]                MONEY          NULL,
+    [Profit]              MONEY          NULL,
+    [Date]                DATETIME       NULL,
+    [Month Number]        FLOAT (53)     NULL,
+    [Month Name]          NVARCHAR (255) NULL,
+    [Year]                NVARCHAR (255) NULL
+);
+
